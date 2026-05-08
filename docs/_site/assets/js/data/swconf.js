9b7c68bac050300604804814ea10c244700c3956
Sarvesh Mishra <191090264+Sarvesh-Mishra1981@users.noreply.github.com>
2026-05-08 18:01:38 +0530

    Update LinkedIn url and remove twitter from contact.yml

--- docs/_site/assets/js/data/swconf.js
+++ docs/_site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1778243131',resources: [
+    cacheName: 'chirpy-1778243489',resources: [
       '/gsoc-blog/assets/css/jekyll-theme-chirpy.css',
       '/gsoc-blog/',
       
